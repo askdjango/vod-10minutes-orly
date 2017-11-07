5de4551f10917e2e8b0ed5bf36f995cdebd472dd
Chinseok Lee <allieuslee@gmail.com>
2017-11-07 21:40:22 +0900

    cover/index 뷰 구현

--- orly/urls.py
+++ orly/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cover/', include('cover.urls', namespace='cover')),
+    url(r'^$', lambda r: redirect('cover:index')),
 ]
